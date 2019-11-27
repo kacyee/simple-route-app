75fe4760104d4dbfd6ffec3e5c2e1f489e1eb766
kacyee <ikacyee@gmail.com>
2019-11-27 19:11:52 +0100

    Updates

---
+++ precache-manifest.5d89de5f0a3fa65b0749a3638ecc61fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "953c8e247c6f291f84a46135f5b15c84",
+    "url": "/simple-route-app/index.html"
+  },
+  {
+    "revision": "beabb1be7be45d413525",
+    "url": "/simple-route-app/static/css/main.1c5ea208.chunk.css"
+  },
+  {
+    "revision": "b95cd45a2c923c1c35af",
+    "url": "/simple-route-app/static/js/2.f9b300f9.chunk.js"
+  },
+  {
+    "revision": "beabb1be7be45d413525",
+    "url": "/simple-route-app/static/js/main.4b84c6f1.chunk.js"
+  },
+  {
+    "revision": "f086d2ab31b07f44bd66",
+    "url": "/simple-route-app/static/js/runtime-main.08a4d793.js"
+  },
+  {
+    "revision": "fdea746ed954f0a21bed7ebcde8140f9",
+    "url": "/simple-route-app/static/media/avatar.fdea746e.jpg"
+  },
+  {
+    "revision": "fa1cca2c39683f9c402e3d85e17454aa",
+    "url": "/simple-route-app/static/media/bg.fa1cca2c.jpeg"
+  }
+]);
